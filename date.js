36bfeda92a748352468a07bd255cd5555510b43a
sahariarahmmad <info.sashakil@gmail.com>
2023-02-25 01:41:23 +0600

    added date confution

---
+++ date.js
@@ -0,0 +1,7 @@
+const date1= new Date('9-12-1999');
+const date2= new Date('9-09-2005');
+if(date1.getTime()>date2.getTime()){
+    console.log("First date is very big");
+}else{
+    console.log("Second date is Big");
+}
--- es6.js
+++ es6.js
@@ -1,1 +1,10 @@
-console.log("done bro!")
+try{
+    let name='shakil';
+console.log(name);
+console.log(nahid);
+} catch(error){
+    console.log(20);
+    console.log(error);
+}finally{
+    console.log("mama valo acho");
+}
