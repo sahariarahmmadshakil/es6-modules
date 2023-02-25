8755761779e74938ebdbfb4f8b063ccd0035a454
sahariarahmmad <info.sashakil@gmail.com>
2023-02-26 00:25:51 +0600

    recap array spreading

--- date.js
+++ date.js
@@ -28,7 +28,26 @@
 // (num1==10)?console.log('true'):console.log('false');
 // finding big number from three numbers by turnary operator
 
-const a=10;
-const b=50;
-const c=30;
-(a>b && a>c)? console.log('A is big'):(b>c && b>a)?console.log('B is Big'):console.log('C is Big');
+// const a=10;
+// const b=50;
+// const c=30;
+// (a>b && a>c)? console.log('A is big'):(b>c && b>a)?console.log('B is Big'):console.log('C is Big');
+
+// array spreading 
+// const numbers=[10,50,60,80,55,402,63];
+// const newNumbers=[105,106,107];
+// console.log(...numbers,...newNumbers);
+// const arr1=[20,30,40];
+// const arr2=[...arr1];
+
+// arr1.push(50);
+// arr2.push(60);
+// console.log(arr1);
+// console.log(arr2);
+
+const arr = [1,2,3,4,5,6,7];
+const [first,second,third,...remaining]=arr;
+console.log(first);
+console.log(second);
+console.log(third);
+console.log(remaining);
