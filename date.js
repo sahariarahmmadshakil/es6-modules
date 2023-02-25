e71b70f815c8d0dc295bccda1f5c059c148922f8
sahariarahmmad <info.sashakil@gmail.com>
2023-02-25 22:58:30 +0600

    recap template string

--- date.js
+++ date.js
@@ -1,7 +1,18 @@
-const date1= new Date('9-12-1999');
-const date2= new Date('9-09-2005');
-if(date1.getTime()>date2.getTime()){
-    console.log("First date is very big");
-}else{
-    console.log("Second date is Big");
-}
+// const date1= new Date('9-12-1999');
+// const date2= new Date('9-09-2005');
+// if(date1.getTime()>date2.getTime()){
+//     console.log("First date is very big");
+// }else{
+//     console.log("Second date is Big");
+// }
+
+
+//Example of Java Script template string 
+// const nm = 'shakil'
+// const age = 20;
+// const output=`My name is ${nm}
+// My age is ${age}
+// I want to learn Programming`
+// console.log(output);
+
+// JavaScript Turnary Operator
